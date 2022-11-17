031775e57718fb06ee06bc5f6f2e88a543a4f68c
Unnar Krist <unnarvc@speedymail.org>
2022-11-17 11:58:29 +0000

    Add preprocessor for type MonoBehaviour

--- Runtime/UnityEngine.MonoBehaviour/MonoBehaviour.Delay.cs
+++ Runtime/UnityEngine.MonoBehaviour/MonoBehaviour.Delay.cs
@@ -1,5 +1,7 @@
 // smidgens @ github
 
+#if !EXT_0_UNITYENGINE_MONOBEHAVIOUR
+
 namespace Smidgenomics.Unity.Extensions
 {
 	using UnityEngine;
@@ -41,3 +43,5 @@ namespace Smidgenomics.Unity.Extensions
 		}
 	}
 }
+
+#endif
