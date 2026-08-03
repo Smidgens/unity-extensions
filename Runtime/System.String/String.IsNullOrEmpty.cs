88b39a92d4fdf4f9edce24896e6cf0a990898925
Unnar <unnarvc@speedymail.org>
2026-08-03 13:13:32 +0000

    Add string.IsNullOrEmpty

---
+++ Runtime/System.String/String.IsNullOrEmpty.cs
@@ -0,0 +1,19 @@
+// smidgens @ github
+
+#if !EXT_0_SYSTEM_STRING
+
+namespace Smidgenomics.Unity.Extensions
+{
+	public static partial class String_
+	{
+		/// <summary>
+		/// Shorthand for string.IsNullOrEmpty(s)
+		/// </summary>
+		public static bool IsNullOrEmpty(this string s)
+		{
+			return string.IsNullOrEmpty(s);
+		}
+	}
+}
+
+#endif
